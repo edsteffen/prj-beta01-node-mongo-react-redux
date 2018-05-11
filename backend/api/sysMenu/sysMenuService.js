8a8bd1e34c65247d977466f36eea5a88043d4074
edsteffen <38445568+edsteffen@users.noreply.github.com>
2018-05-11 11:56:02 -0300

    Testando metodos de agregação

--- backend/api/sysMenu/sysMenuService.js
+++ backend/api/sysMenu/sysMenuService.js
@@ -1,5 +1,33 @@
 const SysMenu = require('./sysMenu')
 
 SysMenu.methods(['get', 'post', 'put', 'delete'])
+SysMenu.updateOptions({ new: true, runValidators: true })
+
+
+SysMenu.route('count', (req, res, next) => {
+    SysMenu.count((error, value) => {
+        if (error) {
+            res.status(500).json({ errors: [error] })
+        } else {
+            res.json({ value })
+        }
+    })
+})
+
+SysMenu.route('summary', (req, res, next) => {
+    SysMenu.aggregate({
+        $project: {
+            item: 1,
+            numDeSubmenus: { $size: "$submens" }
+        }
+    }, (error, result) => {
+            if (error) {
+                res.status(500).json({ errors: [error] })
+            } else {
+                res.json(result || { numDeSubmenus: 0 })
+            }
+        })
+})
+
 
 module.exports = SysMenu
